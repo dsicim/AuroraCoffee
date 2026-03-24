903cd939afae04ae8260445f9b286b0e08491a22
Hüseyin Efe Çağlar <huseyin.caglar@sabanciuniv.edu>
2026-03-24 18:02:31 +0300

    database folder pls

--- Backend/main.js
+++ Backend/main.js
@@ -24,31 +24,7 @@ const server = http.createServer(async function (req, res) {
         res.end("Bad Request");
     }
     else if (req.url.startsWith("/api/")) {
-        const body = await new Promise((resolve, reject) => {
-            let t = 0;
-            let b = "";
-            req.on("data", function (c) {
-                b += c;
-            });
-            req.on("end", function () {
-                if (b == "") {
-                    t = 0;
-                    b = null;
-                }
-                else {
-                    try {
-                        b = JSON.parse(b);
-                        t = 2;
-                    } catch (error) {
-                        t = 1;
-                    }
-                }
-                resolve({ exists: t !== 0, json: t === 2, data: b, err: null });
-            });
-            req.on("error", function (err) {
-                resolve({ exists: false, json: false, data: null, err: err });
-            });
-        });
+        
     }
     else if (req.url.startsWith("/assets/")) {
         console.log("Request for asset: " + req.url.substring(8));
