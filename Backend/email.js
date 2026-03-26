fda7d02b9761c7b463a173832f4c24b4159daa81
Hüseyin Efe Çağlar <huseyin.caglar@sabanciuniv.edu>
2026-03-26 10:22:44 +0300

    i forgot fs and nodemailer here ahhh

--- Backend/email.js
+++ Backend/email.js
@@ -1,4 +1,5 @@
 const nodemailer = require('nodemailer');
+const fs = require('fs');
 const config = JSON.parse(fs.readFileSync("./config.json", "utf-8"));
 
 async function sendEmail(to, subject, body) {
