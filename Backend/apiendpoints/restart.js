c0cd4a6fc2662422d3e09bd4c6d9e83d1b5c18d1
Hüseyin Efe Çağlar <huseyin.caglar@sabanciuniv.edu>
2026-04-02 11:29:55 +0300

    fix

--- Backend/apiendpoints/restart.js
+++ Backend/apiendpoints/restart.js
@@ -3,9 +3,10 @@ const sql = require("../../Database/server.js");
 const fs = require("fs");
 const path = require("path");
 async function handleAPI(config, method, endpoint, query, body, headers, currentUser) {
-    if ((query && Object.keys(query).length && query.key && query.key === config.restarttoken) || (!currentUser.e && currentUser.role === "admin")) {
+    if ((query && Object.keys(query).length && query.key && query.key === config.restarttoken) || (!currentUser.e && currentUser.role === "Admin")) {
         if (method === "GET") {
-            return { s: 200, j: false, d: fs.readFileSync("./restart.html", "utf-8").replaceAll("{inner}",fs.readFileSync("./innerrestart.html", "utf-8")), h: { "Content-Type": "text/html" } };
+            if (endpoint[0] === "getpanel") return { s: 200, j: false, d: fs.readFileSync("./innerrestart.html", "utf-8"), h: { "Content-Type": "text/html" } };
+            else return { s: 200, j: false, d: fs.readFileSync("./restart.html", "utf-8").replaceAll("{inner}",fs.readFileSync("./innerrestart.html", "utf-8")), h: { "Content-Type": "text/html" } };
         }
         else if (method === "POST") {
             if (body && body.exists && body.json && !body.err && body.data.action) {
