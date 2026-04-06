84c65fae5aaf88c6d9c839774f48af97b8c4f316
Hüseyin Efe Çağlar <huseyin.caglar@sabanciuniv.edu>
2026-04-06 19:26:46 +0300

    bin and installments lets go

--- Backend/apiendpoints/payment.js
+++ Backend/apiendpoints/payment.js
@@ -17,18 +17,19 @@ async function IyzipayAPI(config, method, url, headers, body) {
 async function handleAPI(config, method, endpoint, query, body, headers, currentUser) {
     if (endpoint[0] === "carddetails") {
         if (method === "POST") {
-            if (!body || !body.exists || body.err || !body.json || !body.data || !body.data.bin) return { s: 400, j: true, d: { e: "Invalid request body" } };
-            const response = await IyzipayAPI(config, "POST", "payment/bin/check", {}, {locale:"tr",binNumber: body.bin});
-            if (response) {
-                if (response.status === "success") {
-                    return { s: 200, j: true, d: { response: response } };
+            if (!body || !body.exists || body.err || !body.json || !body.data || !body.data.bin || !body.data.price || isNaN(parseFloat(body.data.price))) return { s: 400, j: true, d: { e: "Invalid request body" } };
+            const binresponse = await IyzipayAPI(config, "POST", "payment/bin/check", {}, {locale:"tr",binNumber: body.bin});
+            const insresponse = await IyzipayAPI(config, "POST", "payment/iyzipos/installment", {}, {locale:"tr",price:body.price,binNumber: body.bin});
+            if (binresponse && insresponse) {
+                if (binresponse.status === "success" && insresponse.status === "success") {
+                    return { s: 200, j: true, d: { bin: binresponse, ins: insresponse } };
                 }
                 else {
-                    return { s: 400, j: true, d: { e: "Failed to fetch card details from payment provider", response: response } };
+                    return { s: 400, j: true, d: { e: "Failed to fetch card details from payment provider", bin: binresponse, ins: insresponse } };
                 }
             }
             else {
-                return { s: 500, j: true, d: { e: "An unknown error occurred while communicating with the payment provider", response: response } };
+                return { s: 500, j: true, d: { e: "An unknown error occurred while communicating with the payment provider", bin: binresponse, ins: insresponse } };
             }
         }
         else return { s: 405, j: true, d: { e: "Method Not Allowed" } };
