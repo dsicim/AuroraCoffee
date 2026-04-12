3626006128793d4122ccd928f462f3633ebecf8e
Hüseyin Efe Çağlar <huseyin.caglar@sabanciuniv.edu>
2026-04-12 04:33:15 +0300

    set timeout zero

--- Backend/apiendpoints/payment.js
+++ Backend/apiendpoints/payment.js
@@ -96,7 +96,6 @@ function currencyToSymbol(currency, price) {
     else return symbol + price.toFixed(2);
 }
 async function emailInvoice(config, email, orderNumber, details) {
-    console.log('cwd', process.cwd());
     let instemplate = fs.readFileSync("./"+(details.installment.months === 1 ?"orderemailinfull":"orderemailinstallment")+".html", "utf-8");
     if (details.installment.months > 1) {
         instemplate = instemplate.replaceAll("{{ORDER_TOTAL}}", currencyToSymbol(details.currency, details.price.total))
@@ -656,7 +655,7 @@ async function handleAPI(config, method, endpoint, query, body, headers, current
                                     if (!updateResult.s) return { s: 500, j: true, d: { success: false, e: { what: "Order Confirmation", why: "Order update failed: " + updateResult.e, resolution: "Please contact the developers. YOUR CARD HAS ALREADY BEEN CHARGED" } } };
                                     await completeCart(payload.o.detailsOpen.products);
                                     await sql.clearCart(currentUser.id).then(result => {}).catch(err => {});
-                                    await emailInvoice(config, currentUser.username, orderNumber.n, payload.o.detailsOpen);
+                                    setTimeout(() => emailInvoice(config, currentUser.username, orderNumber.n, payload.o.detailsOpen), 0);
                                     return { s: 200, j: true, d: { success: true, orderNumber: orderNumber.n, response: authChecker } };
                                 }
                                 else return { s: 400, j: true, d: { success: false, e: { what: "Payment Processor", why: "Payment status is not complete yet. Currently showing as " + authChecker.paymentStatus, resolution: "Please wait for a few minutes and check the orders page." } } };
@@ -798,7 +797,7 @@ async function handleAPI(config, method, endpoint, query, body, headers, current
                                     if (!updateResult.s) return CallbackEmbed({ success: false, e: { what: "Order Confirmation", why: "Order update failed: " + updateResult.e, resolution: "Please contact the developers. YOUR CARD HAS ALREADY BEEN CHARGED" } });
                                     await completeCart(details.products);
                                     await sql.clearCart(user).then(result => {}).catch(err => {});
-                                    await emailInvoice(config, email, orderNumber.n, details);
+                                    setTimeout(() => emailInvoice(config, email, orderNumber.n, details), 0);
                                     return CallbackEmbed({ success: true, orderNumber: orderNumber.n, response: authChecker });
                                 }
                                 else return CallbackEmbed({ success: false, e: { what: "Payment Processor", why: "Payment status is not complete yet. Currently showing as " + authChecker.paymentStatus, resolution: "Please wait for a few minutes and check the orders page." } });
