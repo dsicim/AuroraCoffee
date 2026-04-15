98c59d0e3f07272b5dc7e82fec31b83ede39c005
Hüseyin Efe Çağlar <huseyin.caglar@sabanciuniv.edu>
2026-04-15 21:41:36 +0300

    throw the right error please thank you

--- Database/db_functions.js
+++ Database/db_functions.js
@@ -673,6 +673,7 @@ func.setCommentStatus = async function (commentId, status) {
         }
     } catch (error) {
         console.error('Update comment error:', error);
+        if (err instanceof sql.DBError) throw error;
         throw new DBError(500, 'Failed to update comment');
     }
 };
