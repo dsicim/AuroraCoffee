b9545015a5dbe606a022de0f2d002d9492ec447a
Hüseyin Efe Çağlar <huseyin.caglar@sabanciuniv.edu>
2026-04-29 02:52:24 +0300

    privacy setting checks

--- Backend/apiendpoints/users.js
+++ Backend/apiendpoints/users.js
@@ -9,6 +9,15 @@ async function handleAPI(config, method, endpoint, query, body, headers, current
         else if (method === "PATCH") {
             if (!body || !body.exists || body.err || !body.json || !body.data || !body.data.name || !body.data.privacy) return { s: 400, j: true, d: { e: "Invalid request body" } };
             if (body.data.name.trim().split(" ").length < 2) return { s: 400, j: true, d: { e: "Please enter your name and surname" } };
+            const userwords = currentUser.displayname.split(" ").map(s => s.trim()).filter(s => s.length > 0);
+            let privacyinvalid = false;
+            const userprivacy = body.data.privacy.split("").map(s => {
+                if (!["s","h","i"].includes(s)) privacyinvalid = true;
+                return privacyinvalid?null:{"s":"SHOW", "h":"HIDE", "i":"INITIAL"}[s];
+            })
+            if (userprivacy.length === 0) return { s: 400, j: true, d: { e: "Privacy setting is required" } };
+            if (userprivacy.length !== userwords.length) return { s: 400, j: true, d: { e: "Privacy setting length must match the number of words in your display name" } };
+            if (privacyinvalid) return { s: 400, j: true, d: { e: "Invalid privacy setting" } };
             return await sql.editUser(currentUser.id, body.data.name, body.data.privacy).then(res => {
                 if (res.success) {
                     return { s: 200, j: true, d: { e: "User updated successfully" } };
