17d132a0d185815e6983981f22590ea75ac9ae3d
Hüseyin Efe Çağlar <huseyin.caglar@sabanciuniv.edu>
2026-04-29 02:01:36 +0300

    two words needed yes

--- Backend/apiendpoints/users.js
+++ Backend/apiendpoints/users.js
@@ -7,8 +7,9 @@ async function handleAPI(config, method, endpoint, query, body, headers, current
     if (endpoint[0] === "me") {
         if (method === "GET") return { s: 200, j: true, d: { user: currentUser } };
         else if (method === "PATCH") {
-            if (!body || !body.exists || body.err || !body.json || !body.data || !body.data.name || !body.data.nameprivacy) return { s: 400, j: true, d: { e: "Invalid request body" } };
-            return await sql.editUser(currentUser.id, body.data.name, body.data.nameprivacy).then(res => {
+            if (!body || !body.exists || body.err || !body.json || !body.data || !body.data.name || !body.data.privacy) return { s: 400, j: true, d: { e: "Invalid request body" } };
+            if (body.data.name.trim().split(" ").length < 2) return { s: 400, j: true, d: { e: "Please enter your name and surname" } };
+            return await sql.editUser(currentUser.id, body.data.name, body.data.privacy).then(res => {
                 if (res.success) {
                     return { s: 200, j: true, d: { e: "User updated successfully" } };
                 }
