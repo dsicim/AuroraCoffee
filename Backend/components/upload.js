39f8e187ae931d682e92a07a991e1247edca2791
Hüseyin Efe Çağlar <huseyin.caglar@sabanciuniv.edu>
2026-05-05 21:39:57 +0300

    ok now it should work?

--- Backend/components/upload.js
+++ Backend/components/upload.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const sql = require("../../Database/server.js");
 const crypto = require("crypto");
+const stream = require("stream");
 const { loadEsm } = require("load-esm");
 const sharp = require('sharp');
 
@@ -13,14 +14,13 @@ async function createUpload(user, prefName, restrictions, req, headers) {
     if (headers["content-length"] === undefined) return { s: 411, e: "Content-Length header is required" };
     if (isNaN(parseInt(headers["content-length"]))) return { s: 411, e: "Invalid Content-Length header" };
     if (parseInt(headers["content-length"]) > restrictions.maxSize) return { s: 413, e: "File size exceeds the maximum allowed size of " + (restrictions.maxSize / (1024 * 1024)) + " MB" };
-    
     if (prefName.length > 0) prefName = prefName + "-";
-    
     let bytesWritten = 0;
     const { fileTypeFromStream } = await loadEsm("file-type");
-    const ftResult = await fileTypeFromStream(req);
+    const webStream = stream.Readable.toWeb(req);
+    const ftResult = await fileTypeFromStream(webStream);
     const detected = ftResult.fileType;
-    const passthrough = ftResult.stream;
+    const passthrough = stream.Readable.fromWeb(ftResult);
     const uploadprocess = await new Promise((resolve, reject) => {
         if (!detected) {
             passthrough.resume();
