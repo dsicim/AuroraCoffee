0ac9a9d8d5e2c55486d73a758eda4f63ca397e00
Hüseyin Efe Çağlar <huseyin.caglar@sabanciuniv.edu>
2026-05-21 16:15:04 +0300

    seems like for i in something never works.

--- Backend/apiendpoints/wishlist.js
+++ Backend/apiendpoints/wishlist.js
@@ -160,10 +160,11 @@ async function handleAPI(config, method, endpoint, query, body, headers, current
             }
         });
         const emailPromises = [];
-        for (const userId in Object.keys(emailqueue)) {
+        Object.keys(emailqueue).forEach(userId => {
             emailqueue[userId].details = emailqueue[userId].details.filter(d => d.stock > 0);
             if (emailqueue[userId].details.length > 0) emailPromises.push({user_id: userId, ...emailqueue[userId]});
-        }
+        });
+        delete emailqueue;
         res.write(JSON.stringify(emailPromises));
         res.end();
 
