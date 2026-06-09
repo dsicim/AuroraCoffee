037c90724c3d1ba149379e25ce853130e3f7e18b
Hüseyin Efe Çağlar <huseyin.caglar@sabanciuniv.edu>
2026-06-09 20:05:43 +0300

    turns out we already have this

--- Backend/apiendpoints/analytics.js
+++ Backend/apiendpoints/analytics.js
@@ -28,52 +28,6 @@ async function handleAPI(config, method, endpoint, query, body, headers, current
             return { s: 405, j: true, d: { e: "Method Not Allowed" } };
         }
     }
-    else if (endpoint[0] === "sales") {
-        if (method === "GET") {
-            const startDate = query.startDate || null;
-            const endDate = query.endDate || null;
-
-            const allOrders = await sql.getAllOrders(specificorder, startDate, endDate).then(async result => {
-                if (result.success) {
-                    const errors = [];
-                    const orders = result.orders.map(ordr => {
-                        try {
-                            if (specificorder && specificorder != ordr.id) return undefined;
-                            if (specificorder) {
-                                ordr.details = aes.pjs(ordr.details);
-                                if (ordr.details.e && ordr.details.e.startsWith("Failed to parse JSON: ")) throw new Error("Malformed data found on database");
-                                const decrypted = aes.decrypt(ordr.details, ordr.user_id);
-                                if (!decrypted.s) throw new Error("Decryption failed");
-                                const order = aes.pjs(decrypted.value);
-                                if (order.e && order.e.startsWith("Failed to parse JSON: ")) throw new Error("Malformed data found on decrypted database");
-                                ordr.details = order;
-                                if (currentUser.role === "Product Manager") {
-                                    return { order: ordr.details.products };
-                                }
-                            }
-                            else delete ordr.details;
-                            return { order: ordr };
-                        } catch (err) {
-                            console.error("Decrypt order error:", err);
-                            errors.push({ id: ordr.id, e: err.toString() });
-                            return { order: undefined, e: err.toString() };
-                        }
-                    }).filter(ordr => ordr !== undefined);
-                    if (orders.length === 0 && specificorder) return { s: 404, j: true, d: { e: "Order not found" } };
-                    return specificorder ? { s: 200, j: true, d: { order: orders[0] } } : { s: 200, j: true, d: { orders, errors } };
-                }
-                else {
-                    return { s: 400, j: true, d: { e: "An unknown error occurred" } };
-                }
-            }).catch(err => {
-                console.error("Get orders error:", err);
-                if (err instanceof sql.DBError) return { s: err.status, j: true, d: { e: err.error || "An unknown error occurred" } };
-                else return { s: 500, j: true, d: { e: "An unknown error occurred" } };
-            });
-            if (allOrders.s !== 200) return { s: allOrders.s, j: true, d: allOrders.d };
-            return { s: 200, j: true, d: allOrders.d };
-        }
-    }
     return { s: 404, j: true, d: { e: "Not Found" } };
 }
 
